3a828ebd04b81851403403ddcc489f2f9ef9f6e0
anahi1a <anahitagupta2@gmail.com>
2023-04-20 00:07:16 +0530

    nav

---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+import {Navbar, Container} from "react-bootstrap";
+export const NavBar = () => {
+    return (
+        <Navbar expand="lg">
+          <Container>
+            <Navbar.Brand href="#home">
+                <img src={''} alt="Logo"/>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <span className="navbar-toggler-icon"></span>
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#Home">Home</Nav.Link>
+                <Nav.Link href="#About Us">About Us</Nav.Link> 
+                <Nav.Link href="#Contact">Contact</Nav.Link>
+
+
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      )
+}
